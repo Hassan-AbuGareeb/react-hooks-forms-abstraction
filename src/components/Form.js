7d634ef521965bd46a283e448cf030c2847d908c
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-10-21 20:27:34 +0300

    task done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
-
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  function handleFormChange(event) {
+    const key = event.target.name;
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+    setFormData({ ...formData, [key]: [value] });
   }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input
+        type="text"
+        name="firstName"
+        onChange={handleFormChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleFormChange}
+        value={formData.lastName}
+      />
       <button type="submit">Submit</button>
     </form>
   );
